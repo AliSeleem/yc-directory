c376561c06a421e8b03bc7986f069a1b016a0a46
Ali Seleem <engaliseleem@gmail.com>
2024-12-20 00:26:24 +0200

    added hero section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,21 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({ searchParams }: { searchParams: Promise<{query?: string}>}) {
+  const query = (await searchParams).query
   return (
-    <h1>Hello </h1>
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          pitch your startup, 
+          <br /> connect with enterpreneurs
+          </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote in Pitches, and Get Noticed in Virtual Competitions 
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+    </>
   );
 }
