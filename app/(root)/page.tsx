1bfdfde19f7ba0f63ed95b40cdbcb151bc176659
Ali Seleem <engaliseleem@gmail.com>
2024-12-21 23:32:22 +0200

    added startup details, author in sanity & ppr in views

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,16 @@ import SearchForm from "../../components/SearchForm";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { auth } from "@/auth";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{query?: string}>}) {
   const query = (await searchParams).query
 
   const params = { search: query || ""}
+
+  const session = await auth();
+
+  console.log(`Id : ${session?.id}`)
 
   const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
--- auth.ts
+++ auth.ts
@@ -1,6 +1,56 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
+import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries";
+import { client } from "./sanity/lib/client";
+import { writeClient } from "./sanity/lib/writeClient";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	providers: [Github],
+	callbacks: {
+		async signIn({ user: { name, email, image }, profile }) {
+			if (!profile) {
+				throw new Error("Profile is required for sign-in");
+			}
+
+			const { id, login, bio } = profile; // Safe destructuring after check
+
+			const existingUser = await client
+				.withConfig({ useCdn: false })
+				.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+					id,
+				});
+
+			if (!existingUser) {
+				await writeClient.create({
+					_type: "author",
+					id,
+					name,
+					username: login,
+					email,
+					image,
+					bio: bio || "",
+				});
+			}
+
+			return true;
+		},
+
+		async jwt({ token, account, profile }) {
+			if (account && profile) {
+				const user = await client
+					.withConfig({ useCdn: false })
+					.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
+						id: profile?.id,
+					});
+
+				token.id = user?._id;
+			}
+
+			return token;
+		},
+		async session({ session, token }) {
+			Object.assign(session, { id: token.id });
+			return session;
+		},
+	},
 });
